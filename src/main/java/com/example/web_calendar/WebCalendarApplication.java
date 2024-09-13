8aab21eeaf7fe408cc8f5b95a45ff4389a52f636
llvnpys <naysung03@hanyang.ac.kr>
2024-09-13 15:15:51 +0900

    initial commit

---
+++ src/main/java/com/example/web_calendar/WebCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_calendar/WebCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
